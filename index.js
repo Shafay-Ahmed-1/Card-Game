e2629d1981d389fb22a730e121c7aba90936aa6d
Shafay Ahmed <shafayahmed77@gmail.com>
2026-01-24 00:23:35 +0500

    added button ,front page

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let resultEl = document.getElementById("result-el");
 
-if (sum < 20) {
+
+function startgame() {
+    if (sum < 20) {
     message ="Do you want to draw a new card?🙂 ";
 }
 else if (sum === 21) {
@@ -16,4 +19,7 @@ else {
     message = "You're out of the game! 😞 ";
     isAlive = false;
 }
-console.log(message);
+    document.getElementById("card-el").textContent += " " + firstCard + " _ " + secondCard;
+    document.getElementById("sum-el").textContent += " " +sum; 
+    resultEl.textContent = message;
+}
