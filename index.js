859d9e12472095187a38ed19a3dff27799f7d7e1
Shafay Ahmed <shafayahmed77@gmail.com>
2026-01-24 18:28:28 +0500

    setting

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let firstCard = 11;
-let secondCard = 10;
+let secondCard = 9;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let resultEl = document.getElementById("result-el");
-
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("card-el");
 
 function startgame() {
-    if (sum < 20) {
+    if (sum <= 20) {
     message ="Do you want to draw a new card?🙂 ";
 }
 else if (sum === 21) {
@@ -19,7 +20,7 @@ else {
     message = "You're out of the game! 😞 ";
     isAlive = false;
 }
-    document.getElementById("card-el").textContent += " " + firstCard + " _ " + secondCard;
-    document.getElementById("sum-el").textContent += " " +sum; 
+    cardEl.textContent +=  firstCard + "   " + secondCard;
+    sumEl.textContent += sum; 
     resultEl.textContent = message;
 }
